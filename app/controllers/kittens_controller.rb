f5d12cbab38f24ea8e8300e956cce13dab4c61cd
Andrew Chung <andrew.chung159@gmail.com>
2015-04-16 18:58:06 -0400

    Make Kittens resource available via API in .json

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,9 +4,16 @@ class KittensController < ApplicationController
 
 	def index
 		@kittens = Kitten.all
+
+		respond_to do |format|
+			format.json { render :json => @kittens }
+		end
 	end
 
 	def show
+		respond_to do |format|
+			format.json { render :json => @kitten }
+		end
 	end
 
 	def new
